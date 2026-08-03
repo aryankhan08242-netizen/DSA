c4749b24b63dc5fe6d953b7c34a44d285f6a9444
Md Anwar Alam <aryankhan08242@gmail.com>
2026-08-03 18:27:53 +0530

    Sorting Zero and One in Vector

---
+++ Array2/SortingArray.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sort01(vector<int>a){
+int no0=0;
+int no1=0;
+for(int i=0;i<a.size()-1;i++){
+    if(a[i]==0){
+        no0++;
+    }
+    else{
+        no1++;
+    }
+}
+for(int i=0;i<a.size();i++){
+    if(i<no0){
+        a[i]=0;
+    }
+    else{
+        a[i]=1;
+    }
+}
+for(int i=0;i<a.size()-1;i++){
+    cout<<a[i]<<" ";
+}
+cout<<endl;
+}
+
+int main(){
+    vector<int>v;
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(1);
+    for(int i=0; i<v.size()-1;i++){
+        cout<<v[i]<<" ";
+
+    }
+    cout<<endl;
+    sort01(v);
+}
