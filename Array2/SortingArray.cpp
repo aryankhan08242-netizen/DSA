4e83510700aae3ab1d017341b138e5470feb51d3
Md Anwar Alam <aryankhan08242@gmail.com>
2026-08-04 17:28:19 +0530

    Finding Maximum Element in 2D Array

--- Array2/SortingArray.cpp
+++ Array2/SortingArray.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void sort01(vector<int>a){
 int no0=0;
 int no1=0;
-for(int i=0;i<a.size()-1;i++){
+for(int i=0;i<a.size();i++){
     if(a[i]==0){
         no0++;
     }
@@ -21,7 +21,7 @@ for(int i=0;i<a.size();i++){
         a[i]=1;
     }
 }
-for(int i=0;i<a.size()-1;i++){
+for(int i=0;i<a.size();i++){
     cout<<a[i]<<" ";
 }
 cout<<endl;
@@ -37,7 +37,7 @@ int main(){
     v.push_back(1);
     v.push_back(1);
     v.push_back(1);
-    for(int i=0; i<v.size()-1;i++){
+    for(int i=0; i<v.size();i++){
         cout<<v[i]<<" ";
 
     }
