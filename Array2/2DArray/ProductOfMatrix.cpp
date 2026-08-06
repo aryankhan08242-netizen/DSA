0e9164df5c3c84baf64885d5784861f2c87c8eef
Md Anwar Alam <aryankhan08242@gmail.com>
2026-08-06 23:50:06 +0530

    Linear Search in array

---
+++ Array2/2DArray/ProductOfMatrix.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of Rows in 1 first Matrix"<<endl;
+    cin>>n;
+    int m;
+    cout<<"Enter the number of Column in 1 first Matrix"<<endl;
+    cin>>m;
+    int p,q;
+    cout<<"Enter the number of Rows in 1 first Matrix"<<endl;
+    cin>>p;
+    int mat1[n][m];
+    int mat2[p][q];
+    cout<<"Enter the element of matrix 1";
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>mat1[i][j];
+        }
+    }
+    cout<<"Enter the element of matrix 2";
+    for(int i=0;i<p;i++){
+        for(int j=0;j<m;q++){
+            cin>>mat1[i][j];
+        }
+    }
+    if(n==p){
+        int product
+      for
+    }
+    else{
+        cout<<"It should be the matrix 1 rowa is not equal to the column of second number";
+    }
+
+}
---
+++ Array2/2DArray/RotationOfMatrice.c++
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void Rotation(vector<vector<int> &matrix,)
+
+int main(){
+
+}
