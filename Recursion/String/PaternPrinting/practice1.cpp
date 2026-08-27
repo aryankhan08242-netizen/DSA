23ce7841735e16d125fa053aab531d26ed54b5d7
Md Anwar Alam <aryankhan08242@gmail.com>
2026-08-27 16:47:31 +0530

    Back Tracking Usinf Recursion

--- Recursion/String/PaternPrinting/practice1.cpp
+++ Recursion/String/PaternPrinting/practice1.cpp
@@ -6,7 +6,7 @@ void pattern1(int n){
         for(int j=0;j<n-i-1;j++){
             cout<<" ";
         }
-        for(int j=0;j<2*i+1;j++){
+        for(int j=0;j<2*i+1;j--){
         cout<<"*";
         }
         
