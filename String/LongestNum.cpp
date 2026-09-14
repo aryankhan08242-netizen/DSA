95362771fbc9cc23aa65495a91dd5b96027c7ea5
Md Anwar Alam <aryankhan08242@gmail.com>
2026-09-14 14:40:41 +0530

    LeetCode 14

--- String/LongestNum.cpp
+++ String/LongestNum.cpp
@@ -1,7 +1,34 @@
 #include<iostream>
 #include<string>
+#include<vector>
 #include<algorithm>
 using namespace std;
 int main(){
-    
+    vector<string>v;
+    v.push_back("flight");
+    v.push_back("flower");
+    v.push_back("flow");
+    int n=v.size();
+    sort(v.begin(),v.end());
+    for(int i=0; i<n;i++){
+        cout<<v[i]<<endl;
+    }
+    string first=v[0];
+    string last=v[n-1];
+    string s="";
+    for(int i=0;i<min(first.size(),last.size());i++){
+        if(first[i]==last[i]){
+            s+=first[i];
+        }
+        else{
+            break;
+        }
+        
+    }
+     cout<<s;
+     return 0;
+
+
+
+
 }
