75aa53cda371786ef2378fdf6d026676f30282df
Md Anwar Alam <aryankhan08242@gmail.com>
2026-09-20 23:59:03 +0530

    Selectio sorting

---
+++ BubbleSort/practice2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<climits>
+using namespace std; int main(){
+    int arr[7]={9, 4, 7, 2, 8, 1, 5};
+    // sort index 2 to 5
+    for(int i=2; i<6; i++){
+        int min=INT_MAX;
+        int mindex=-1;
+        for(int j=i; j<6;j++){
+           if(arr[j]<min){
+             min=arr[j];
+            mindex=j;
+           }
+
+        }
+        swap(arr[i],arr[mindex]);
+
+    }
+    for(int i=0; i<8;i++){
+        cout<<arr[i]<<" ";
+    }
+}
