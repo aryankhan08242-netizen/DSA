441089c1974d84c296ea15ebd58c640aaef91691
Md Anwar Alam <aryankhan08242@gmail.com>
2026-09-23 00:05:23 +0530

    Finding Largest Element in Array

--- BubbleSort/practice3.cpp
+++ BubbleSort/practice3.cpp
@@ -3,8 +3,13 @@
 #include<algorithm>
 using namespace std;
 int main(){
-    int arr[5]={5,2,1,3,4};
-    for(int i=0; i<4; i++){
+    int n; cout<<"Enter the value of n";
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<5; i++){
         int min=INT_MAX;
         int mindx=-1;
         for(int j=i; j<5;j++){
@@ -13,10 +18,31 @@ int main(){
                 mindx=j;
             }
         }
-        swap(arr[i],min);
+        swap(arr[i],arr[mindx]);
 
     }
     for(int i=0; i<5; i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
+    int max;
+    int max_el=INT_MIN;
+    for(int i=0 ;i<n; i++){
+if(arr[i]>max_el){
+    max_el=arr[i];
+}
+    }
+    cout<<max_el<<endl;
+    for(int i=0; i<5; i++){
+        arr[i]=-(arr[i]-max_el);
+    }
+      for(int i=0; i<5; i++){
+       cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    reverse(arr, arr +n);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    
 }
